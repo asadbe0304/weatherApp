4107378e15b6922667df6819a92ddc884f56e419
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-04-02 16:37:12 +0500

    Fixes

--- src/context/reducer.tsx
+++
@@ -1,9 +0,0 @@
-export const Reducer = (state:State, action:Action):State => {
-  switch (action.type) {
-    case "weather":
-      return {
-        ...state,
-        open: false,
-      };
-  }
-};
